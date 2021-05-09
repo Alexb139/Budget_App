303cb5a8edb6cb03ca21983d511a89e5ecc443cb
Alexandria Baynes <alexandriadbaynes@gmail.com>
2021-05-09 17:15:50 -0500

    inital commit

---
+++ classbudget.py
@@ -0,0 +1,18 @@
+class Category:
+
+    def __init__(self, category, amount):
+        self.category = category
+        self.amount = amount
+
+    #Methods
+    def deposit(self):
+        return "This is a deposit method"
+
+    def check_balance(self):
+        pass
+
+    def withdraw(self):
+        pass
+
+    def transfer(self):  
+        pass           
